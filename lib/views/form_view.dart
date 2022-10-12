c1e81c79fce2d6f58c55f3d382f4b33be147be2a
narumi <tarakosuziko0610@gamil.com>
2022-10-12 23:54:42 +0900

    入力フォームの高さを変更

--- lib/views/form_view.dart
+++ lib/views/form_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sufree/views/home_view.dart';
 
 class FormView extends StatefulWidget {
   const FormView({super.key});
@@ -9,6 +10,7 @@ class FormView extends StatefulWidget {
 
 class _FormViewState extends State<FormView> {
   late TextEditingController _controller;
+
   @override
   void initState() {
     super.initState();
@@ -17,17 +19,37 @@ class _FormViewState extends State<FormView> {
 
   @override
   Widget build(BuildContext context) {
+    _controller.selection = TextSelection.fromPosition(
+      const TextPosition(offset: 0), //カーソルの位置を0にすることでカーソルの位置を頭に設定
+    );
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _controller,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('心の底からはきだしてみよう', style: TextStyle(fontSize: 16)),
+            SizedBox(height: 8),
+            Container(
+              height: 500,
+              child: TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                // 親Widgetに合わせて高さを変える
+                expands: true,
+                minLines: null,
+                maxLines: null,
+                textAlignVertical: TextAlignVertical.top,
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 8),
+            ElevatedButton(
+                onPressed: () => Navigator.pop(context), child: Text('成仏する'))
+          ],
+        ),
       ),
     );
   }
